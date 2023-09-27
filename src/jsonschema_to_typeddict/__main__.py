b099a0d3e03968f92b699b990f7bbc1c09e5194d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-09-27 20:41:53 +0300

    Formatting

--- src/jsonschema_to_typeddict/__main__.py
+++ src/jsonschema_to_typeddict/__main__.py
@@ -6,7 +6,7 @@ from . import converter
 
 def create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--output-path", type=Path, required=True,help="Path to the generated stub file")
+    parser.add_argument("--output-path", type=Path, required=True, help="Path to the generated stub file")
     parser.add_argument("--root-name", type=str, required=True, help="Name of the root argument")
     parser.add_argument("schema_path", type=Path, help="Path to the jsonschema file")
     return parser
