19ce7ded7214b6e0ae032a883b8a2ed4f417425b
duncathan <dunc@duncathan.com>
2024-12-19 13:50:00 -0600

    use typing.Literal for enum schemas
    also slightly cleans up the output format

--- tests/sample_schema.pyi
+++ tests/sample_schema.pyi
@@ -7,22 +7,45 @@ import typing_extensions as typ
 
 
 # Definitions
-
 @typ.final
 class DictDefinition(typ.TypedDict, total=False):
     nested_ref: ScenarioName
     number: int
 
+ScenarioName = typ.Literal[
+    's010_cave',
+    's020_magma',
+    's030_baselab',
+    's040_aqua',
+    's050_forest',
+    's060_quarantine',
+    's070_basesanc',
+    's080_shipyard',
+    's090_skybase'
+]
 
-ScenarioName: typ.TypeAlias = str
+UnionEnum = typ.Literal[
+    'foo',
+    2403,
+    12.34,
+    None
+]
 
 
 # Schema entries
 class SampleFieldBItem(typ.TypedDict, total=False):
     something: float
 
+SampleFieldCPickupType = typ.Literal[
+    'actor',
+    'emmi',
+    'corex',
+    'corpius',
+    'cutscene'
+]
+
 class SampleFieldC(typ.TypedDict, total=False):
-    pickup_type: typ.Required[str]
+    pickup_type: typ.Required[SampleFieldCPickupType]
     model: list[str]
     pickup_lua_callback: str
     pickup_actordef: str
@@ -30,6 +53,6 @@ class SampleFieldC(typ.TypedDict, total=False):
 
 @typ.final
 class Sample(typ.TypedDict):
-    field_a: dict[str, str]
+    field_a: dict[ScenarioName, str]
     field_b: typ.NotRequired[list[SampleFieldBItem]]
     field_c: SampleFieldC
